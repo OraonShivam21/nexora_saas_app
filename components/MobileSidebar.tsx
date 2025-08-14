566f3a09a005e5bcfbc01fc035490a7bae65a756
OraonShivam21 <oraonshivam.0302@gmail.com>
2025-08-14 23:35:30 +0530

    resolved hydration error

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import Sidebar from "./Sidebar";
+import Sidebar from "@/components/Sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <Sheet>
-      <SheetTrigger>
+      <SheetTrigger asChild>
         <Button variant="ghost" size="icon" className="md:hidden">
           <Menu />
         </Button>
